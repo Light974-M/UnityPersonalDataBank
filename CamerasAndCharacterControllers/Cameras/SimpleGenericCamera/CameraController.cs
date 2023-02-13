252a6ed2a3e822e34fda71e727025079cfca096d
maximilien vitiello <maxiquick@outlook.fr>
2023-02-13 15:32:00 +0100

    add final camera controller

---
+++ CamerasAndCharacterControllers/Cameras/SimpleGenericCamera/CameraController.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace UPDB.CamerasAndCharacterControllers.Cameras.SimpleGenericCamera
+{
+    /// <summary>
+    /// usefull generic camera controller, can be used with fps or tps controller or alone(or wathever controller you want, simply drag script into object that will be rotated)
+    /// </summary>
+    [AddComponentMenu("UPDB/CamerasAndCharacterControllers/Cameras/SimpleGenericCamera/Generic Camera Controller")]
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField, Tooltip("choose the input mode")]
+        private InputMode _input;
+
+        [SerializeField, Tooltip("speed of mouse look in X and Y")]
+        private Vector2 _lookSpeed = Vector2.one;
+
+        #region Private API
+
+        /// <summary>
+        /// main variable to setup input value each update
+        /// </summary>
+        private Vector2 _inputValue = Vector2.zero;
+
+        /// <summary>
+        /// used to store rotation that will be offset by input each update
+        /// </summary>
+        private Vector2 _rotation = Vector2.zero;
+
+        #endregion
+
+        #region Public API
+
+        public Vector2 InputValue
+        {
+            get { return _inputValue; }
+            set { _inputValue = value; }
+        }
+
+        public enum InputMode
+        {
+            Standalone,
+            InputSystem,
+        }
+
+        #endregion
+
+        /// <summary>
+        /// awake is called when script instance is loaded
+        /// </summary>
+        private void Awake()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        /// <summary>
+        /// update is called each frame
+        /// </summary>
+        private void Update()
+        {
+            if (_input == InputMode.Standalone)
+                _inputValue = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+            Look();
+        }
+
+        /// <summary>
+        /// manage rotation of transform, following input value
+        /// </summary>
+        private void Look()
+        {
+            Vector2 mouse = new Vector2(_inputValue.x * _lookSpeed.x, _inputValue.y * _lookSpeed.y);
+            _rotation += new Vector2(-mouse.y, mouse.x);
+
+            _rotation.x = Mathf.Clamp(_rotation.x, -89, 89);
+
+            transform.eulerAngles = new Vector3(_rotation.x, _rotation.y, 0.0f);
+
+            _inputValue = Vector2.zero;
+        }
+
+        /// <summary>
+        /// public function to put input value with input system callback system
+        /// </summary>
+        /// <param name="callback">value of input system callback, represent a Vector2(for mouse x and y)</param>
+        public void GetLook(InputAction.CallbackContext callback)
+        {
+            if (_input == InputMode.InputSystem)
+                _inputValue = callback.ReadValue<Vector2>();
+        }
+    }
+}
--- CoreHelpers/UsableMethods/UPDBBehaviour.cs
+++ CoreHelpers/UsableMethods/UPDBBehaviour.cs
@@ -18,5 +18,135 @@ namespace UPDB
             return variable != null;
         }
 
+        /// <summary>
+        /// make a lerp automatically(based on delta time update rate) with float, between a and b, in a specific time
+        /// </summary>
+        /// <param name="a">start point</param>
+        /// <param name="b">end point</param>
+        /// <param name="lerpTime">time to make lerp</param>
+        /// <param name="timer">timer to store lerp progression</param>
+        /// <returns></returns>
+        private static float AutoLerp(float a, float b, float lerpTime, ref float timer)
+        {
+            float value = 0;
+
+            if (timer < lerpTime)
+            {
+                value = Mathf.Lerp(a, b, timer / lerpTime);
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                timer = lerpTime;
+                value = b;
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// make a lerp automatically(based on delta time update rate) with Vector2, between a and b, in a specific time
+        /// </summary>
+        /// <param name="a">start point</param>
+        /// <param name="b">end point</param>
+        /// <param name="lerpTime">time to make lerp</param>
+        /// <param name="timer">timer to store lerp progression</param>
+        /// <returns></returns>
+        private static Vector2 AutoLerp(Vector2 a, Vector2 b, float lerpTime, ref float timer)
+        {
+            Vector2 value = Vector2.zero;
+
+            if (timer < lerpTime)
+            {
+                value = Vector2.Lerp(a, b, timer / lerpTime);
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                timer = lerpTime;
+                value = b;
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// make a lerp automatically(based on delta time update rate) with Vector3, between a and b, in a specific time
+        /// </summary>
+        /// <param name="a">start point</param>
+        /// <param name="b">end point</param>
+        /// <param name="lerpTime">time to make lerp</param>
+        /// <param name="timer">timer to store lerp progression</param>
+        /// <returns></returns>
+        private static Vector3 AutoLerp(Vector3 a, Vector3 b, float lerpTime, ref float timer)
+        {
+            Vector3 value = Vector3.zero;
+
+            if (timer < lerpTime)
+            {
+                value = Vector3.Lerp(a, b, timer / lerpTime);
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                timer = lerpTime;
+                value = b;
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// make a lerp automatically(based on delta time update rate) with Vector4, between a and b, in a specific time
+        /// </summary>
+        /// <param name="a">start point</param>
+        /// <param name="b">end point</param>
+        /// <param name="lerpTime">time to make lerp</param>
+        /// <param name="timer">timer to store lerp progression</param>
+        /// <returns></returns>
+        private static Vector4 AutoLerp(Vector4 a, Vector4 b, float lerpTime, ref float timer)
+        {
+            Vector4 value = Vector4.zero;
+
+            if (timer < lerpTime)
+            {
+                value = Vector4.Lerp(a, b, timer / lerpTime);
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                timer = lerpTime;
+                value = b;
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// make a rotation lerp automatically(based on delta time update rate) with Quaternions, between a and b, in a specific time
+        /// </summary>
+        /// <param name="a">start point</param>
+        /// <param name="b">end point</param>
+        /// <param name="lerpTime">time to make lerp</param>
+        /// <param name="timer">timer to store lerp progression</param>
+        /// <returns></returns>
+        private static Quaternion AutoLerp(Quaternion a, Quaternion b, float lerpTime, ref float timer)
+        {
+            Quaternion value = Quaternion.identity;
+
+            if (timer < lerpTime)
+            {
+                value = Quaternion.Lerp(a, b, timer / lerpTime);
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                timer = lerpTime;
+                value = b;
+            }
+
+            return value;
+        }
+
     } 
 }
